a765ab514b4289b4456afbe1993df120ff718971
cs8898 <cs8898@gmx.de>
2020-03-31 15:46:49 +0200

    fixed link generation

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ln -s /tmp/conf/nginx /etc/nginx/modules-enabled/90-socket-site.conf &&
+[ ! -f /etc/nginx/nodules-enabled/90-socket-site.conf ] &&
+	ln -s /tmp/conf/nginx /etc/nginx/modules-enabled/90-socket-site.conf
 service nginx start &&
-setsid openfortivpn -c /tmp/conf/forti
+	setsid openfortivpn -c /tmp/conf/forti
+
